d180ea6b73a44e519980106dac7465bddbcd0447
Sina Ghadri <info@sinaghadri.com>
2024-05-26 12:57:19 +0330

    initial: add initial project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import EtherComponent from "./components/base/ether";
+import Web3Component from "./components/base/web3";
+
+function App() {
+  return (
+    <>
+      <Web3Component />
+      <EtherComponent />
+    </>
+  );
+}
+
+export default App;
